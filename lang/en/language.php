923f4c0f2b411a918f86f2ad85912f30985951b9
Dennis Iversen <dennis.iversen@gmail.com>
2015-09-18 20:27:43 +0200

    lang

---
+++ lang/en/language.php
@@ -0,0 +1,10 @@
+<?php
+
+$LANG = array();
+
+// Translation of file htdocs/templates/openid-selector/common.php
+
+$LANG['Let me stay logged in'] = 'Let me stay logged in';
+$LANG['days'] = 'days';
+$LANG['Log in with openID'] = 'Log in with openID';
+$LANG['Choose provider'] = 'Choose provider';
